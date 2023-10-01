9112f25be686b4a21bd827ea9c5e02a2160f0014
max <frantic33@yandex.ru>
2023-10-01 15:26:24 +0300

    Seventh lesson( filtering by user and course)

--- lms_project/learning/forms.py
+++ lms_project/learning/forms.py
@@ -6,4 +6,4 @@ class CourseForm(forms.ModelForm):
 
     class Meta:
         model = Course
-        fields = ('title', 'description', 'start_date', 'duration', 'price', 'count_lessons')
+        fields = ('title', 'description', 'start_date', 'duration', 'price', 'count_lessons',)
