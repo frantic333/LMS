4669bfcef12ce452121b287d807879e529bfb97b
max <frantic33@yandex.ru>
2023-10-22 21:46:22 +0300

    Fourteenth lesson(Using annotate+values in learning/signals/check_quantity)

--- lms_project/learning/signals.py
+++ lms_project/learning/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import pre_save
 from django.dispatch import Signal
 from .models import Course, Lesson
+from django.db.models import Count
 
 
 set_views = Signal()
@@ -8,15 +9,11 @@ set_views = Signal()
 
 def check_quantity(sender, instance, **kwargs):
     error = None
-    optimal_query = sender.objects.select_related('course').filter(course=instance.course)
-    actual_count = optimal_query.count()
-    set_count = optimal_query.first.course.count_lessons
-#    actual_count = sender.objects.filter(course=instance.course).count()
-#    set_count = Course.objects.filter(id=instance.course.id).values('count_lessons')[0]['count_lessons']
+    course_data = Course.objects.filter(id=instance.course.id).annotate(lessons_count=Count('lessons')).values('count_lessons', 'lessons_count')
 
-    if actual_count >= set_count:
+    if course_data.lessons_count >= course_data.count_lessons:
         error = f'Количество уроков ограничено!' \
-                f'Ранее вы установили, что курс будет содержать {set_count} уроков'
+                f'Ранее вы установили, что курс будет содержать {course_data.count_lessons} уроков'
     return error
 
 
