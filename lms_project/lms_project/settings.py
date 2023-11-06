414f9e5f27145fd89a9516dcdc999d062b1b8b33
max <frantic33@yandex.ru>
2023-11-06 23:19:52 +0300

    Eighteenth lesson(test_post_register_view_with_email_existed)

--- lms_project/lms_project/settings.py
+++ lms_project/lms_project/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-op624$qbun18dr_su_%&hf3e0jaa6zx0hl+k%fce9hd2=&4x8i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -110,6 +110,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('PASSWORD_DB'),
         'NAME': 'lms_project',
         'ATOMIC_REQUEST': True,
+        'TEST': {
+            'NAME': 'lms_project_test',
+        },
     }
 }
 
